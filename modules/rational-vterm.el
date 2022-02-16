09f2b5a4f47d26110a3a9db2a95fb6922af7d9d9
Willem H. Bos <whbos@xs4all.nl>
2022-02-16 12:25:18 +0100

    Add vterm and vterm-extra

---
+++ modules/rational-vterm.el
@@ -0,0 +1,8 @@
+(straight-use-package 'vterm)
+
+(straight-use-package '(vterm-extra
+                        :type git
+                        :host github
+                        :repo "Sbozzolo/vterm-extra"))
+
+(provide 'rational-vterm)
