069c4d2884f9b94f8c41551efb0564d1a89bf51b
Willem H. Bos <whbos@xs4all.nl>
2022-02-20 16:04:12 +0100

    Add Yasnippet

---
+++ modules/rational-yasnippet.el
@@ -0,0 +1,9 @@
+;;; rational-yasnippet.el -*- lexical-binding: t; -*-
+
+(straight-use-package 'yasnippet)
+
+(straight-use-package 'yasnippet-snippets)
+
+(yas-global-mode 1)
+
+(provide 'rational-yasnippet)
