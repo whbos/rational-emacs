b91042c85aba0e5683e496d917916eb58dab10b5
Willem H. Bos <whbos@xs4all.nl>
2022-03-02 16:28:36 +0100

    modules/rational-git-auto-commit-mode.el

---
+++ modules/rational-git-auto-commit-mode.el
@@ -0,0 +1,19 @@
+;;; rational-git-auto-commit-mode.el -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022
+;; SPDX-License-Identifier: MIT
+
+;; Author: Willem H. Bos
+
+;; Commentary
+
+;; General sane defaults
+
+;;; Code:
+
+(straight-use-package 'git-auto-commit-mode)
+
+(git-auto-commit-mode)
+
+(provide 'rational-git-auto-commit-mode)
+;;; rational-git-auto-commit-mode.el ends here
